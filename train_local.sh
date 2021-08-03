90a40be2471105ffd8a61fbe98f327f90756eeb7
Keith Azzopardi <keith_azzopardi@hotmail.com>
2021-08-03 19:06:20 +0200

    renaming training bash script

--- train_local.sh
+++ train_local.sh
@@ -1,7 +1,7 @@
 export HDF5_DISABLE_VERSION_CHECK=1
-python train.py --lr 0.001 \
+python3 train.py --lr 0.001 \
                 --batch_size 32 \
                 --seq_len 5 \
                 --num_days_test 60 \
-                --dataset_path ./od_matrix_100pc_20x5.npy \
-                --out_path ./train-100pc_20x5
+                --dataset_path ./od_matrix_100pc_10x3.npy \
+                --out_path ./testing_python3
