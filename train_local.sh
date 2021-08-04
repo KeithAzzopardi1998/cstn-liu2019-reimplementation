92623e3816511a4afa740094f8b3c3039dc5f85b
Keith Azzopardi <keith_azzopardi@hotmail.com>
2021-08-04 09:59:51 +0200

    updating output directory name

--- train_local.sh
+++ train_local.sh
@@ -4,4 +4,4 @@ python3 train.py --lr 0.001 \
                 --seq_len 5 \
                 --num_days_test 60 \
                 --dataset_path ./od_matrix_100pc_10x3.npy \
-                --out_path ./testing_python3
+                --out_path ./train-python3_test
